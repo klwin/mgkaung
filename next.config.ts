248c537b2f6bbcb091c748c0fb0ab40cd0904994
Kaung Lwin <klwin@easysheets.net>
2025-01-05 18:28:09 -0700

    init: build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 };
 
 export default nextConfig;
